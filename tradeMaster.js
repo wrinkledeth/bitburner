39279248f4a2d16bec01ff0745e567b33b6b6b04
zen <wrinkled.eth@gmail.com>
2022-01-11 13:44:28 -0500

    Functional Trading Bot

---
+++ tradeMaster.js
@@ -0,0 +1,18 @@
+/** @param {import(".").NS} ns **/
+export async function main(ns) {
+    const host = ns.getHostname();
+    const symbols = ns.stock.getSymbols();
+    const symlist = [];
+    for (let sym of symbols){
+        let price = ns.stock.getPrice(sym);
+        let maxShares = ns.stock.getMaxShares(sym);
+        let product = price * maxShares;
+        symlist.push({'sym': sym, 'max': product});
+    };
+    symlist.sort((b,a)=> (a.max - b.max));
+    for (let sym of symlist){
+        let symbol = sym['sym']
+        ns.exec('trader.js', host, 1, symbol)
+    }
+
+}
---
+++ trader.js
@@ -0,0 +1,45 @@
+/** @param {import(".").NS} ns **/
+export async function main(ns) {
+    const sym = ns.args[0]; //FLCM, WDS
+    const maxShares = ns.stock.getMaxShares(sym);
+    let holding = false;
+    let shares;
+    let cost;
+    while (true) {
+        await ns.sleep(6000);
+        let forecast = ns.stock.getForecast(sym);
+        //ns.tprint(forecast);
+        if (forecast > .6 & !holding) {
+            ns.tprint('------------------');
+            ns.tprint(`Forecast % = ${forecast}`)
+            shares, cost = buy(sym)
+            holding = true
+        }
+        if (forecast < .54 & holding) {
+            ns.tprint('------------------');
+            ns.tprint(`Forecast % = ${forecast}`);
+            sell(sym, shares, cost);
+            holding = false;
+        }
+    }
+
+    function buy(sym) {
+        const ask = ns.stock.getAskPrice(sym);
+        const money = ns.getPlayer().money;
+        shares = Math.min(money / ask, maxShares);
+        cost = ask * shares
+        ns.tprint(`buying ${ns.nFormat(shares,'0.00a')} shares of ${sym} at ${ns.nFormat(ask,'$0.00a')} price`);
+        ns.stock.buy(sym, shares);
+        //ns.tprint(`cash = ${ns.nFormat(ns.getPlayer().money, '$0.00a')}`);
+        return shares, cost;
+    }
+
+    function sell(sym, shares, cost) {
+        const bid = ns.stock.getBidPrice(sym);
+        ns.tprint(`selling ${ns.nFormat(shares,'0.00a')} shares of ${sym} at ${ns.nFormat(bid,'$0.00a')} price`)
+        ns.stock.sell(sym, shares);
+        const profit = ns.nFormat(bid * shares - cost, '$0.00a');
+        ns.tprint(`PROFIT = ${profit}`);
+        ns.tprint(`cash = ${ns.nFormat(ns.getPlayer().money, '$0.00a')}`);
+    }
+}
